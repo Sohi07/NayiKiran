d9aadbb86cf6839bec623d5f4e613d448ab4371b
Sohini Das <sohinid474@gmail.com>
2026-06-12 15:00:16 +0530

    Made changes to Signup UI

--- frontend/src/pages/signup/Signup.jsx
+++ frontend/src/pages/signup/Signup.jsx
@@ -21,7 +21,7 @@ const Signup = () => {
         <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
             <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10">
                 <h1 className="text-3xl font-semibold text-center text-gray-400">
-                    Signup<span className="text-blue-900"> Harmoni</span>
+                   <span className="text-blue-900">Signup</span>
                 </h1>
 
                 <form onSubmit={handleSubmit}>
